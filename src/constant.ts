c7e6c399455e7ccc11e5f4ce572bec7b58f45d73
qiuqfang <2312180277@qq.com>
2023-02-13 11:42:06 +0800

    chore: 给案例添加一些配置

--- src/constant.ts
+++ src/constant.ts
@@ -2,10 +2,11 @@ import { RollupPluginSwcOption } from "./index";
 
 export const DEFAULT_OPTIONS: RollupPluginSwcOption = {
   jsc: {
+    loose: false, // 通常不推荐启用
     parser: {
       syntax: "typescript",
     },
-    target: "esnext",
+    target: "esnext", // 编译成哪个版本
   },
-  module: { type: "es6" },
+  module: { type: "es6" }, // 编译后代码的模块化方案
 };
